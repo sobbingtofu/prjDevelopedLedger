e53f7bc1bebead421d589ae7dc627fa35d6e00ba
sobbingtofu <kukubasak@naver.com>
2024-06-13 01:16:24 +0900

    update: main.jsx에 queryclient 걸어주기

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import {Provider} from "react-redux";
 import App from "./App.jsx";
 import "./index.css";
 import store from "./redux/config/configStore.js";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </QueryClientProvider>
 );
